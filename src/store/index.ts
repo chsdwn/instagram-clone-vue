8765812a5c190495e1a6942ac2f882b948aebb9b
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-08-12 14:09:29 +0300

    Initial commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,4 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>Home</h1>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
   name: "Home",
-  components: {
-    HelloWorld
-  }
 });
 </script>
+
+<style scoped>
+h1 {
+  color: red;
+
+  @media (--t) {
+    color: blue;
+  }
+}
+</style>
