d88fba00943a114eaf912d5556c89dc4c40eae89
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-08-13 15:41:35 +0300

    Reset CSS and SVG Icons

---
+++ src/components/CustomText.vue
@@ -0,0 +1,40 @@
+<template>
+  <component :is="tag" :class="size">
+    <slot />
+  </component>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+const CustomText = Vue.extend({
+  name: 'CustomText',
+  props: {
+    tag: {
+      type: String,
+      default: 'span',
+    },
+    size: {
+      type: String,
+      default: 'normal', // normal: 14, small: 12, xsmall: 11, xxsmall: 10
+      validator: function(value: string) {
+        return ['normal', 'small', 'xsmall', 'xxsmall'].indexOf(value) !== -1;
+      },
+    },
+  },
+});
+
+export default CustomText;
+</script>
+
+<style scoped>
+.small {
+  font-size: 12px;
+}
+.xsmall {
+  font-size: 11px;
+}
+.xxsmall {
+  font-size: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,37 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <CustomText class="a1" tag="p">Hello World2</CustomText>
+    <CustomText class="a2" tag="p" size="small">Hello World3</CustomText>
+    <CustomText class="a3" tag="p" size="xsmall">Hello World4</CustomText>
+    <CustomText tag="p" size="xxsmall">Hello World5</CustomText>
+    <IconHome width="60" height="60" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
+import CustomText from '@/components/CustomText';
+
+import IconHome from '../icons/home.svg';
 
 export default Vue.extend({
-  name: "Home",
+  name: 'Home',
+  components: {
+    CustomText,
+    IconHome,
+  },
 });
 </script>
 
 <style scoped>
-h1 {
-  color: red;
-
-  @media (--t) {
-    color: blue;
-  }
+.a1 {
+  color: rgb(var(--a72));
+}
+.a2 {
+  color: rgb(var(--b3f));
+}
+.a3 {
+  color: rgb(var(--ba8));
 }
 </style>
